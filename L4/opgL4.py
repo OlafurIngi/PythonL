c598a15c927b321049ed77f909f228cb96ba03f9
Olafur <olafuringieinarsson@gmail.com>
2023-09-27 10:02:40 +0200

    Update opgL4.py

--- L4/opgL4.py
+++ L4/opgL4.py
@@ -40,4 +40,24 @@ def write_data():
     fileThree.close()
 
 
-write_data()
+#   write_data()
+
+
+def fibonacci(n):
+    result = 0
+    value_one = 0
+    value_two = 1
+    for i in range(1, n):
+        n = value_one + value_two
+        value_one = value_two
+        value_two = n
+        result += 1
+
+        print(value_two)
+
+
+#   fibonacci(7)
+
+
+
+
